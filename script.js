719d15bbb60c6ea1e6f3f6cff4b3ba7a64f546c6
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-21 10:21:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-// Function to create the table
+
 function createTable() {
-    const numRows = prompt("Input number of rows:");
-    const numColumns = prompt("Input number of columns:");
+    const rows = prompt("Input number of rows");
+    const columns = prompt("Input number of columns");
     const table = document.getElementById("myTable");
 
     // Clear any existing content in the table
     table.innerHTML = '';
-
-    // Create the table rows and cells based on user input
-    for (let i = 0; i < numRows; i++) {
+    for (let i = 0; i < rows; i++) {
         const row = table.insertRow(i);
 
-        for (let j = 0; j < numColumns; j++) {
+        for (let j = 0; j < columns; j++) {
             const cell = row.insertCell(j);
             cell.textContent = `Row-${i} Column-${j}`;
         }
     }
 }
-
-// // Attach the createTable function to the button click event
-// const createTableButton = document.getElementById("createTableButton");
-// createTableButton.addEventListener("click", createTable);
